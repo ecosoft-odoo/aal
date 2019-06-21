da7d6efde6a04a130e03d13beafdab64f4c89d07
ps-tubtim <pimolnat.s@outlook.com>
2019-06-21 17:38:23 +0700

    [12.0][ADD]aal_purchase_form

---
+++ aal_purchase_form/__manifest__.py
@@ -0,0 +1,22 @@
+# Copyright 2019 Ecosoft Co., Ltd (http://ecosoft.co.th/)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html)
+
+{
+    'name': 'aal_Purchase_Form',
+    'version': '12.0.1.0.0',
+    'author': 'Ecosoft',
+    'license': 'AGPL-3',
+    'website': 'https://github.com/ecosoft-odoo/aal',
+    'category': 'Report',
+    'depends': [
+        'web',
+        'purchase',
+        'partner_fax',
+    ],
+    'data': [
+        'data/report_data.xml',
+        'reports/purchase_order_form.xml',
+        'reports/purchase_style.xml',
+    ],
+    'installable': True,
+}
