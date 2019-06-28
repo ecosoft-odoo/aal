531c7d8c5d27be2a04ff51105521082e9c9a724c
ps-tubtim <pimolnat.s@outlook.com>
2019-06-28 16:07:48 +0700

    [12.0][fix]account_invoice

---
+++ account_invoice_line_sale_info/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2019 Ecosoft Co., Ltd (http://ecosoft.co.th/)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html)
+
+{
+    'name': 'Account invoice line sale info',
+    'version': '12.0.1.0.0',
+    'category': 'Generic Modules/Accounting',
+    'author': 'Ecosoft, Odoo Community Association (OCA)',
+    'website': 'https://github.com/OCA/account-invoicing',
+    'license': 'AGPL-3',
+    'depends': [
+        'account',
+    ],
+    'data': [
+        'views/account_invoice_view.xml',
+    ],
+    'installable': True,
+    'maintainers': ['Saran440']
+}
---
+++ account_invoice_line_sale_info/readme/DESCRIPTION.rst
@@ -0,0 +1,2 @@
+When created invoice from sale order. Each line in invoice not show reference source document.
+This module allows to show source document on the invoice lines.
