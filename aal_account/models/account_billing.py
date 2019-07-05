cdea05f8f0b6416d3a176e853c35d88397b4a3d5
ps-tubtim <pimolnat.s@outlook.com>
2019-07-05 13:58:58 +0700

    [12.0][FIX]aal_account

--- aal_account/models/account_billing.py
+++ aal_account/models/account_billing.py
@@ -9,8 +9,11 @@ class AccountBilling(models.Model):
         string='Subtotal',
         compute='_compute_amount',
     )
-    amount_wht = fields.Float(
-        string='Withholding Tax',
+    amount_diff_name = fields.Char(
+        default='Withholding Tax',
+        help='change name',
+    )
+    amount_diff = fields.Float(
         digits=dp.get_precision('Account'),
     )
     amount_total = fields.Float(
@@ -18,7 +21,7 @@ class AccountBilling(models.Model):
         compute='_compute_amount',
     )
 
-    @api.depends('invoice_ids', 'amount_wht')
+    @api.depends('invoice_ids', 'amount_diff')
     def _compute_amount(self):
         self.subtotal = sum(self.invoice_ids.mapped('amount_total'))
-        self.amount_total = self.subtotal - self.amount_wht
+        self.amount_total = self.subtotal + self.amount_diff
